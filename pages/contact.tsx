c1c136dc3678c0ccc6f25049b8080a8218d1ca27
kaceykniffen <kaceykniffen@gmail.com>
2022-12-28 15:05:39 -0600

    Added styling for donate section on Contact Page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { FormEvent } from "react";
 import MainLayout from "../components/layouts/MainLayout";
+import Image from "next/image";
 
 import styles from "./Contact.module.scss";
 
@@ -41,25 +42,22 @@ export default function Contact() {
 						/>
 						<button type="submit">Submit</button>
 					</form>
-					<form className={styles.contactForm}>
-						<h2>Contact Us</h2>
-						<label htmlFor="name">Name:</label>
-						<input
-							type="text"
-							id="name"
-							name="name"
-							placeholder="e.g. John Doe"
-						/>
-						<label htmlFor="email">Email:</label>
-						<input
-							type="email"
-							id="email"
-							name="email"
-							placeholder="e.g. youraddress@email.com"
-						/>
-						<label htmlFor="message">Message:</label>
-						<textarea id="message" name="message" />
-					</form>
+					<div className={styles.donate}>
+						<h2>Donate</h2>
+						<div className={styles.imageWrapper}>
+							<Image
+								src="/images/dojo-dreamers.png"
+								fill
+								style={{ objectFit: "cover" }}
+								alt="Dojo Dreamers"
+							/>
+						</div>
+						<p>
+							I love making music for you all, and if you love my music, please
+							feel free to help me out
+						</p>
+						<button>Donate</button>
+					</div>
 				</section>
 			</MainLayout>
 		</>
