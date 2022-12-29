79b9d6080d6e623171ca77e75156d987abe83cd2
kaceykniffen <kaceykniffen@gmail.com>
2022-12-29 15:45:39 -0600

    Added Music and Store Pages

---
+++ pages/music.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import MainLayout from "../components/layouts/MainLayout";
+
+export default function Music() {
+	return (
+		<>
+			<Head>
+				<title>Noveliss | Music</title>
+			</Head>
+			<MainLayout>Music Page</MainLayout>
+		</>
+	);
+}
---
+++ pages/store.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import MainLayout from "../components/layouts/MainLayout";
+
+export default function Store() {
+	return (
+		<>
+			<Head>
+				<title>Noveliss | Store</title>
+			</Head>
+			<MainLayout>Store Page</MainLayout>
+		</>
+	);
+}
