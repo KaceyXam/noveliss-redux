dcb8b470c5d3d79adaa53ff193ed324301b210b1
kaceykniffen <kaceykniffen@gmail.com>
2023-02-12 19:04:34 -0600

    Remove Wave animation because it didn't look good

--- components/header/Wave.tsx
+++ components/header/Wave.tsx
@@ -1,27 +1,17 @@
 import styles from "./Wave.module.scss";
 import SvgLoader from "../svg/SvgLoader";
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Wave() {
-  return (
-    <header className={styles.wrapper}>
-      <h1 className={styles.title}>Noveliss</h1>
-      <div className={styles.title} aria-hidden>
-        <SvgLoader height={90} fill="black" />
-      </div>
-      <div className="imageWrapper">
-        <Image 
-          src="/images/wave/wave.webp" 
-          alt="Hero Image" 
-          fill 
-        />
-      </div>
-      <Image 
-        src="/images/wave/wave-element.webp" 
-        alt="Hero Image" 
-        className={styles.waveElement}
-        fill 
-      />
-    </header>
-  )
+	return (
+		<header className={styles.wrapper}>
+			<h1 className={styles.title}>Noveliss</h1>
+			<div className={styles.title} aria-hidden>
+				<SvgLoader height={90} fill="black" />
+			</div>
+			<div className="imageWrapper">
+				<Image src="/images/wave/wave.webp" alt="Hero Image" fill />
+			</div>
+		</header>
+	);
 }
